2a04991529750be09db9163e5bb5b69aa9e2d93c
1waytwitch-ui <1waytwitch@gmail.com>
2025-12-09 12:12:02 +0100

    Update backtestengine.py

--- backtestengine.py
+++ backtestengine.py
@@ -510,12 +510,12 @@ fig.add_trace(go.Scatter(
 
 
 fig.add_vline(
-    x=price_low,
+    x=P_lower,
     line=dict(color="green", width=2, dash="dot"),
     name="Range Low"
 )
 fig.add_annotation(
-    x=price_low,
+    x=P_lower,
     y=max(IL_curve),
     text="Low",
     showarrow=False,
@@ -523,14 +523,13 @@ fig.add_annotation(
     yshift=10
 )
 
-
 fig.add_vline(
-    x=price_high,
+    x=P_upper,
     line=dict(color="green", width=2, dash="dot"),
     name="Range High"
 )
 fig.add_annotation(
-    x=price_high,
+    x=P_upper,
     y=max(IL_curve),
     text="High",
     showarrow=False,
@@ -539,12 +538,12 @@ fig.add_annotation(
 )
 
 fig.add_vline(
-    x=price_deposit,
+    x=P_deposit,
     line=dict(color="blue", width=2, dash="dash"),
     name="Price Deposit"
 )
 fig.add_annotation(
-    x=price_deposit,
+    x=P_deposit,
     y=min(IL_curve),
     text="Deposit",
     showarrow=False,
@@ -553,12 +552,12 @@ fig.add_annotation(
 )
 
 fig.add_vline(
-    x=current_price,
+    x=P_now,
     line=dict(color="purple", width=2),
     name="Price Now"
 )
 fig.add_annotation(
-    x=current_price,
+    x=P_now,
     y=min(IL_curve),
     text="Now",
     showarrow=False,
