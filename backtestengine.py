286d72838b24f4449b2e854b9c183274c7f78939
1waytwitch-ui <1waytwitch@gmail.com>
2026-03-04 12:53:14 +0100

    Update backtestengine.py

--- backtestengine.py
+++ backtestengine.py
@@ -1,3 +1,14 @@
+import streamlit as st
+import requests
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt
+import plotly.graph_objects as go
+import plotly.express as px
+import plotly.io as pio
+import yfinance as yf
+import math
+
 # ---- MASQUER MENU ET FOOTER ----
 st.markdown("""
 <style>
@@ -13,18 +24,6 @@ footer {visibility: hidden;}
 }
 </style>
 """, unsafe_allow_html=True)
-
-
-import streamlit as st
-import requests
-import numpy as np
-import datetime
-import matplotlib.pyplot as plt
-import plotly.graph_objects as go
-import plotly.express as px
-import plotly.io as pio
-import yfinance as yf
-import math
 
 # ===================== CONFIG PAGE =====================
 st.set_page_config(
